6491e8be5350582be843a93e6949fd8e3a5fef57
Mark48853 <sathanalao@hotmail.com>
2022-08-10 23:59:27 +0700

    add tasks

---
+++ Tasks/Body_Surface_Area.py
@@ -0,0 +1,12 @@
+from math import sqrt, log10
+w=float(input())
+h=float(input())
+
+# Mosteller
+print(sqrt(w*h)/60)
+
+# Havcock
+print(0.024265 * w ** 0.5378 * h ** 0.3964)
+
+# Boyd
+print(0.0333 * (w ** (0.6157 - 0.0188*(log10(w))) * (h ** 0.3) ))
---
+++ Tasks/Stirling_Factorial.py
@@ -0,0 +1,9 @@
+from math import pi,e,sqrt
+
+def stirling(n):
+    print((sqrt(2*pi)) * (n ** (n+1/2)) * (e ** (-n+(1/(12*n+1)))))
+    print((sqrt(2*pi)) * (n ** (n+1/2)) * (e ** (-n+(1/(12*n)))))
+    return n
+
+n = int(input())
+stirling(n)
