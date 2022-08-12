e970a54cf835c810909adc9e28c9f4197aa52573
Mark48853 <sathanalao@hotmail.com>
2022-08-12 21:33:18 +0700

    add task

---
+++ Tasks/AddVector.py
@@ -0,0 +1,7 @@
+a = input().strip('[]').split(', ')
+b = input().strip('[]').split(', ')
+
+a = list(map(float,a))
+b = list(map(float,b))
+
+print('{} + {} = {}'.format(a, b, list(map(lambda x, y: x+y, a, b))))
